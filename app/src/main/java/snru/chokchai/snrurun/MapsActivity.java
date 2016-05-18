d62ffcbe2642130abffc46abbd08ff7f89b9b667
belle <bel.bel.love@hotmail.co.th>
2016-05-18 00:12:16 -0700

    mack  maps ได้แล้ว18/5/2559

--- app/src/main/java/snru/chokchai/snrurun/MapsActivity.java
+++ app/src/main/java/snru/chokchai/snrurun/MapsActivity.java
@@ -11,8 +11,10 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
-
+     // Explicit
     private GoogleMap mMap;
+    private double snruLatADouble=17.191968,
+            snruLngADouble = 104.092300;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,25 +24,17 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-    }
+    }// Main method
 
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-    }
-}
+       // Setup for สกล
+        LatLng snruLatLng = new LatLng(snruLatADouble, snruLngADouble);
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(snruLatLng,15));
+
+
+    } // On maps
+}// Main Class
